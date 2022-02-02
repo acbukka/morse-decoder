69e52525c0e085df5445cafe7a3ed82bea8b351d
acbukka <thesweylis@gmail.com>
2022-02-02 18:08:23 +0700

    (SOS)

--- src/index.js
+++ src/index.js
@@ -34,11 +34,36 @@ const MORSE_TABLE = {
     '--...':  '7',
     '---..':  '8',
     '----.':  '9',
-    '-----':  '0',
+    '-----': '0',
+    '**********': ' '
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(encode) {
+    let encodeArr = [];
+    let encodeNewArr = [];
+    let filteredArr = [];
+    let outStr = '';
+    for (let i = 0; i < encode.length; i += 10) {
+      encodeArr.push(encode.substring(i, i + 10));
+    }
+    for (let j = 0; j < encodeArr.length; j++) {
+      if (encodeArr[j].length < 10) {
+        encodeNewArr.push(encodeArr[j].padStart(10, '0'));
+        console.log(encodeArr[j].length);
+      } else {
+        encodeNewArr.push(encodeArr[j]);
+      }
+    }
+    console.log(encodeNewArr);
+    for (let m = 0; m < encodeNewArr.length; m++) {
+      filteredArr.push(encodeNewArr[m].replaceAll('10', '.').replaceAll('11', '-').replaceAll('0', ''));
+    }
+    console.log(filteredArr);
+    for (let p = 0; p < filteredArr.length; p++) {
+      outStr += MORSE_TABLE[filteredArr[p]];
+      console.log(outStr);
+    }
+    return outStr;
 }
 
 module.exports = {
